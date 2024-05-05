77e373f047faab7d6acde23717b251d2b5d8417c
Gabulon <gabo.fajardo7@gmail.com>
2024-05-05 11:57:51 -0600

    Extrayendo Valores de la consulta previa

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,14 +1,20 @@
-export default function useClima(){
-    const obtenerClima = ({cuidad, pais}) =>{
-        //importar el Api Key
-        const key =import.meta.env.VITE_API_KEY
-        //obtener lat y long
-        const url=`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1=&appid=${key}`
-            console.log(url);
+import axios from "axios";
+export default function useClima() {
+  const obtenerClima =async ({ cuidad, pais }) => {
+    //importar el Api Key
+    const key = import.meta.env.VITE_API_KEY;
 
-    }
+    try {
+      //obtener lat y long
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1=&appid=${key}`;
+      
+      const {data} =await axios(url)
+      const{lat,lon}=data[0]
+      
+    } catch (error) {}
+  };
 
-    return{
-        obtenerClima
-    }
+  return {
+    obtenerClima,
+  };
 }
