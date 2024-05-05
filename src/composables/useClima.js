f8793313040165cbbfef9d99cc71e777fe3edf7b
Gabulon <gabo.fajardo7@gmail.com>
2024-05-05 11:45:11 -0600

    primer llamado hacia la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -2,7 +2,9 @@ export default function useClima(){
     const obtenerClima = ({cuidad, pais}) =>{
         //importar el Api Key
         const key =import.meta.env.VITE_API_KEY
-
+        //obtener lat y long
+        const url=`http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1=&appid=${key}`
+            console.log(url);
 
     }
 
